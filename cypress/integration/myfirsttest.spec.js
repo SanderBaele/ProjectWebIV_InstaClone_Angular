99624ba2250edbd8a611d9945144551c6dc2c34e
Sander Baele <sander.baele@student.hogent.be>
2019-05-23 17:37:17 +0200

    testen Afgewerkt

--- cypress/integration/myfirsttest.spec.js
+++ cypress/integration/myfirsttest.spec.js
@@ -4,14 +4,8 @@ describe('Alle testen', function () {
   
 
   it('De app runt', function () {
-
     cy.visit('')
-    
- 
-
   });
-
-
 
   it('De posts kunnen opgehaald worden', function() {
     cy.server(
@@ -23,6 +17,25 @@ describe('Alle testen', function () {
      
     });
   
+  });
+  it('De gebruiker logt met correcte gegevens en de get request mock response geeft 1 post terug', function() {
+    cy.server();
+    cy.route({
+      method: 'GET',
+      status:200,
+      url: '/api/posts',
+      response: 'fixture:posts.json' 
+    });
+    cy.visit('/login');
+    cy.get('[data-cy=login-email]').clear();
+    cy.get('[data-cy=login-email]').type('student@hogent.be');
+    cy.get('[data-cy=login-password]').clear();
+    cy.get('[data-cy=login-password]').type('P@ssword1111');
+    cy.get('[data-cy=login-button]').click();
+  
+    cy.get('[data-cy=postCard]').should('have.length',1);
+    
+   
   });
   it('De gebruiker logt met correcte gegevens in en bevind zich op de post-list pagina', function() {
     cy.server();
@@ -43,25 +56,7 @@ describe('Alle testen', function () {
     
    /* cy.get('[data-cy=postCard]').should('have.length',1);*/
   });
-  it('De gebruiker logt met correcte gegevens en de get request krijgt 2 posts binnen', function() {
-    cy.server();
-    cy.route({
-      method: 'GET',
-      status:200,
-      url: 'https://localhost:44393/api/posts',
-      response: [{id:2, caption:'De verwachte caption'}]   
-    });
-    cy.visit('/login');
-    cy.get('[data-cy=login-email]').clear();
-    cy.get('[data-cy=login-email]').type('student@hogent.be');
-    cy.get('[data-cy=login-password]').clear();
-    cy.get('[data-cy=login-password]').type('P@ssword1111');
-    cy.get('[data-cy=login-button]').click();
-  
-    cy.get('[data-cy=postCard]').should('have.length',2);
-    
-   /* cy.get('[data-cy=postCard]').should('have.length',1);*/
-  });
+
   it('Op naam klikken in navbar navigeert naar profiel', function() {
  
     /*INLOGGEN*/
@@ -125,38 +120,53 @@ describe('Alle testen', function () {
     
    
   });
+  it('Alle posts van de user kunnen verwijderd worden en het aantal is dan ook 0', function() {
+     
+    cy.server();
+    cy.visit('/login');
+    cy.get('[data-cy=login-email]').type('student@hogent.be');
+    cy.get('[data-cy=login-password]').clear();
+    cy.get('[data-cy=login-password]').type('P@ssword1111');
+    cy.get('[data-cy=login-button]').click();
+    cy.get('[data-cy=fotoToevoegen]').click();
+    cy.url().should('include', '/add-post')
+    cy.get('[data-cy=caption]').type('Een tekst voor de caption');
+    cy.get('[data-cy=deelKnop]').click();
+    cy.url().should('include', '/post-list')
+    
+    cy.get('[data-cy=profielButton]').click();
+  
+    cy.get('[data-cy=verwijderButton]').click({force:true,multiple:true});
+    cy.get('[data-cy=userPosts]').should('have.length',0);
+  
+  
+    
+   
+  });
+  it('Uitloggen navigeert naar loginscherm', function() {
+     
+    cy.server();
+    cy.visit('/login');
+    cy.get('[data-cy=login-email]').type('student@hogent.be');
+    cy.get('[data-cy=login-password]').clear();
+    cy.get('[data-cy=login-password]').type('P@ssword1111');
+    cy.get('[data-cy=login-button]').click();
+    cy.get('[data-cy=fotoToevoegen]').click();
+    cy.url().should('include', '/add-post')
+    cy.get('[data-cy=caption]').type('Een tekst voor de caption');
+    cy.get('[data-cy=deelKnop]').click();
+    cy.url().should('include', '/post-list')
+    
+    cy.get('[data-cy=logoutKnop]').click();
+  
+    cy.url().should('include', '/login')
+   
+  
+    
+   
+  });
 
-  /*  it('filter works', function() {
-      cy.visit('/');
-      cy.get('[data-cy=filterInput]').type('sp');
-      cy.get('[data-cy=recipeCard]').should('have.length', 1);
-    });
   
-    it('mock recipe get', function() {
-      cy.server();
-      cy.route({
-        method: 'GET',
-        url: '/api/recipes',
-        status: 200,
-        response: 'fixture:recipes.json'
-      });
-  
-      cy.visit('/');
-      cy.get('[data-cy=recipeCard]').should('have.length', 3);
-      cy.get('[data-cy=appError]').should('not.be.visible');
-    });
-  
-    it('on error should show error message', function() {
-      cy.server();
-      cy.route({
-        method: 'GET',
-        url: '/api/recipes',
-        status: 500,
-        response: 'ERROR'
-      });
-      cy.visit('/');
-      cy.get('[data-cy=appError]').should('be.visible');
-    });*/
 });
 
 
